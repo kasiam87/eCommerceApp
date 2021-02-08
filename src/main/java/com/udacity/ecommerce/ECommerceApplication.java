718c06dd89d201e650e87818649daeec9bb93396
Katarzyna Skocka <katarzyna.skocka@vimn.com>
2021-02-08 19:49:42 +0100

    Add mvn dependecies

--- src/main/java/com/udacity/ecommerce/ECommerceApplication.java
+++ src/main/java/com/udacity/ecommerce/ECommerceApplication.java
@@ -3,11 +3,12 @@ package com.udacity.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.udacity.ecommerce.model.persistence.repositories")
 @EntityScan("com.udacity.ecommerce.model.persistence")
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
