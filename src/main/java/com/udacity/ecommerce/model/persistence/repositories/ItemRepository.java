71127fc9ddc9876a5edff380fe909ba04111b1fe
Katarzyna Skocka <katarzyna.skocka@vimn.com>
2021-02-08 19:38:56 +0100

    Add gitignore and rename packages

---
+++ src/main/java/com/udacity/ecommerce/model/persistence/repositories/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.udacity.ecommerce.model.persistence.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.udacity.ecommerce.model.persistence.Item;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+	public List<Item> findByName(String name);
+
+}
